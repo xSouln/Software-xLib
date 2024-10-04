5e70f930b2b916d89debbf0ea152f340f8a5e355
xSouln <90866478+xSouln@users.noreply.github.com>
2024-10-04 13:50:42 +0300

    common update

--- Common/Json.cs
+++ Common/Json.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using xLibV100.Components;
 
 namespace xLibV100.Common
@@ -13,29 +12,6 @@ namespace xLibV100.Common
         private static void trace(string str)
         {
             Tracer?.Invoke(str);
-        }
-
-        public static string FileExtensionClear(string file_name)
-        {
-            string[] str = file_name.Split('.');
-            int i = str.Length;
-
-            while (i > 0)
-            {
-                if (str[i - 1] != "json")
-                {
-                    break;
-                }
-                i--;
-            }
-
-            file_name = "";
-            for (int j = 0; j < i; j++)
-            {
-                file_name += str[j];
-            }
-
-            return file_name + ".json";
         }
 
         public static bool CreateFolder(string path, string subpath)
@@ -88,26 +64,22 @@ namespace xLibV100.Common
                     JsonSerializer.Serialize(stream, arg, inType, options);
                     trace("json file is save:\r" + fileName);
                     stream.Close();
-                    return -1;
                 }
             }
             catch (Exception e)
             {
                 trace("json file save error:\r" + e);
+                return -1;
             }
-            return 0;
-        }
 
-        public static int Open<TObject>(string fileName, out TObject arg)
-        {
-            return Open(fileName, out arg, null);
+            return 0;
         }
 
         public static object Deserialize(string fileName, Type outType)
         {
             if (fileName == null)
             {
-                trace("json file open error: file_name = null");
+                trace("json file open error: fileName = null");
                 return null;
             }
 
@@ -140,17 +112,17 @@ namespace xLibV100.Common
             return null;
         }
 
-        public static int Open<TObject>(string file_name, out TObject arg, JsonConverter converter)
+        public static int Open<TObject>(string fileName, out TObject arg)
         {
             arg = default(TObject);
 
-            if (file_name == null)
+            if (fileName == null)
             {
-                trace("json file open error: file_name = null");
+                trace("json file open error: fileName = null");
                 return -1;
             }
 
-            if (!File.Exists(file_name))
+            if (!File.Exists(fileName))
             {
                 trace("json file open error: file not found");
                 return -1;
@@ -158,21 +130,16 @@ namespace xLibV100.Common
 
             try
             {
-                using (FileStream stream = new FileStream(file_name, FileMode.Open))
+                using (FileStream stream = new FileStream(fileName, FileMode.Open))
                 {
                     var options = new JsonSerializerOptions();
                     options.PropertyNameCaseInsensitive = true;
                     options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
                     options.AllowTrailingCommas = true;
 
-                    if (converter != null)
-                    {
-                        options.Converters.Add(converter);
-                    }
-
                     var result = JsonSerializer.DeserializeAsync<TObject>(stream, options);
                     arg = result.Result;
-                    trace("json file is open:\r" + file_name);
+                    trace("json file is open:\r" + fileName);
                     return 0;
                 }
             }
